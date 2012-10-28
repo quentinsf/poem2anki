715bc531317c438a75f2b6f7274d13b083fd98a7
Quentin Stafford-Fraser <quentin@pobox.com>
2012-10-28 10:31:27 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# poem2anki
+
+_Create ANKI flashcards from text files of poems_
+
+* In the desktop Anki, click 'Deck' to take you to the list of decks, then click 'Import'
+
+* Pick the file you just generated and make sure that the type is set to 'Text file separated by tabs or semicolons' and open it.
+
+* Click on the 'Deck' button to choose the deck to which they should be added, or to create a new one.
+
+* When that's done, check that Anqi has detected that the fields are separated by tabs, and click import.
+
+* For best effect, read the lines out loud to yourself when studying the cards.  Spoken words are much more memorable than read ones.
---
+++ poem2anki.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+#
+# poem2anki.py
+#
+# Convert a text file into Anki flash cards.
+# The question will be a certain number of lines, (default 3)
+# and the response is the following line or lines (default 1).
+# The source is read from stdin and the CSV for importing into
+# Anki is sent to stdout.
+#
+# e.g.
+#    ./poem2anki.py < sonnet18.txt > sonnet18.csv
+#
+
+import argparse
+import sys
+from collections import deque
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--qlines", type=int, default=3, 
+        help="No. of lines in question")
+    parser.add_argument("-a", "--alines", type=int, default=1, 
+        help="No. of lines in answer")
+
+    args = parser.parse_args()
+    qlines = args.qlines
+    alines = args.alines
+
+    # Take out blank lines
+    poem = [rl.strip() for rl in sys.stdin.readlines() 
+                  if len(rl.strip()) > 0]
+
+    questions = deque([""] * qlines)
+    answers = deque([""] * alines)
+
+    def cardshift(next_line):
+        """
+        We move the card through the poem by adding the next line
+        to the answer and dropping the first line of the question.
+        """
+        answers.append(next_line)
+        questions.append(answers.popleft())
+        questions.popleft()
+
+    line_reader = iter(poem)
+
+    # Fill up the first card
+    # We need one question line plus however many answer lines
+    try:
+        for i in range(1 + alines):
+            cardshift(line_reader.next())
+    except StopIteration:
+        print >>sys.stderr, "Insufficient lines for a single card"
+        sys.exit()
+
+    # Then print cards until we run out of input lines
+    try:
+        while True:
+            print "<br>".join(questions), "\t", "<br>".join(answers)
+            cardshift(line_reader.next())
+    except StopIteration:
+        pass
+
+
+if __name__ == '__main__':
+    main()
